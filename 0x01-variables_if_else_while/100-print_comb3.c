8e3f8ef56bb5e6cc3a5265fd1d474d94fffe7197
ayaelas <assalharras@gmail.com>
2023-06-16 06:13:24 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int y;
+
+	for (y = 48; y < 58; y++)
+	{
+		putchar(b);
+		if (y != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
