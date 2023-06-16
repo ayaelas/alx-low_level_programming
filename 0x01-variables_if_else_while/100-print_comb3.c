f9c6499f8a1fcbafb57bd700e0fa62e02825311c
ayaelas <assalharras@gmail.com>
2023-06-16 06:53:27 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-	int a,b;
+	int a, m;
 
 	for (a = 48; a <= 56; a++)
 	{
-		for (b = 49; b <= 57; b++)
+		for (m = 49; m <= 57; m++)
 		{
-			if (b > a)
+			if (m > a)
 			{
 				putchar(a);
-				putchar(b);
-				if (a != 56 || b != 57)
+				putchar(m);
+				if (a != 56 || m != 57)
 				{
 					putchar(',');
 					putchar(' ');
