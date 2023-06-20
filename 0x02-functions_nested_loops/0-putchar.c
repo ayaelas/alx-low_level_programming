7b9ab6f90f67f25e56eacd37ced6c458cb79557b
ayaelas <assalharras@gmail.com>
2023-06-20 06:50:27 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  *is written by mbah
  * Return: 0
  */
-int main(void){
+int main(void) {
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -15,4 +15,4 @@ int main(void){
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-	}
+}
