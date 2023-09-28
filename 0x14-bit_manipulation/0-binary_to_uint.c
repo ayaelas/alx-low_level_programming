150012afbeb1ca58db788f2044b17a6f2c0fe52b
ayaelas <assalharras@gmail.com>
2023-09-28 08:37:49 -0700

    r

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,10 +12,10 @@ unsigned int binary_to_uint(const char *b)
 	}
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' || b[i] || '1')
-			{
-				return (0);
-			}
+		if (b[i] != '0' || b[i] != '1')
+		{
+			return (0);
+		}
 		converted_num = 2 * converted_num + ( b[i] - '0')
 
 	}
