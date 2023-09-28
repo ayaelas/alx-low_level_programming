fc92a3cecc33de381c06003f1fcd1b865953dc85
ayaelas <assalharras@gmail.com>
2023-09-28 08:32:44 -0700

    try

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int binary_to_uint(const char *b)
+{
+	int converted_num;
+
+	if (!b)
+	{
+		return NULL;
+	}
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != '0' || b[i] || '1')
+			{
+				return (0);
+			}
+		converted_num = 2 * converted_num + ( b[i] - '0')
+
+	}
+	return (converted_num);
+}
